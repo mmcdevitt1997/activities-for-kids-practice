4dd0c83f14e3160d6abfdd836268bea3b063fc78
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-08-27 13:18:03 -0500

    first commit

---
+++ activities-for-kids.py
@@ -0,0 +1,30 @@
+
+running_kids = ["Pam", "Sam", "Andrea", "Will"]
+swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+
+def run():
+    for runner in running_kids:
+        print(f"{runner} ran like sonic")
+
+def swing():
+    for swinger in swinging_kids:
+        print(f"{swinger} swinging too high")
+
+def slide():
+    for slider in sliding_kids:
+        print(f"{slider} down the slide!")
+
+def hide():
+    for hider in hiding_kids:
+        print(f"{hider} you can't find them!")
+
+run()
+swing()
+slide()
+hide()
+
+
+
+
